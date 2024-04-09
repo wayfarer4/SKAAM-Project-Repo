78350d418220c8f11ed2b5b8c127ad371ed2046b
Aniket Chaudhry <chaudhryaniket@gmail.com>
2024-04-09 18:56:10 -0400

    Pushed some SQL statements as comments to begin writing into Python routes

--- flask-app/src/locations/locations.py
+++ flask-app/src/locations/locations.py
@@ -4,3 +4,35 @@ from src import db
 
 
 students = Blueprint('students', __name__)
+
+# -----
+#SELECT *
+#FROM Space WHERE IsAvailable = true; -- Viewing availability; 1.2
+
+# ------
+
+# -----
+#UPDATE BookingDetails
+#SET CheckedIn = '2024-04-03 03:30:00'
+#WHERE BookingId = 4; -- Check in; 1.3
+
+# -------
+
+# -------
+#UPDATE BookingDetails
+#SET BookingNameEvent = 'Study Session'
+#WHERE BookingId = 4; -- Change Booking Name; 1.4
+#---------
+
+# ------
+#UPDATE BookingDetails
+#SET BookingLength = '01:30:00'
+#WHERE BookingId = 4; -- Extend the booking; 1.5
+# ------
+
+# ------
+# DELETE FROM BookingDetails
+# WHERE BookingId = 1; -- Cancel booking; 1.6
+
+
+# -----
