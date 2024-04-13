1f47cefcf28a535fe112cea0cdbdfd496741e089
amulyajaya <149338775+amulyajaya@users.noreply.github.com>
2024-04-13 17:42:17 -0400

    spaces route 2 insert

--- flask-app/src/spaces/spaces.py
+++ flask-app/src/spaces/spaces.py
@@ -26,11 +26,46 @@ def get_avail_spaces():
 
 #INSERT INTO Space (SpaceId, BuildingId)
 #   VALUES (4, 2); -- Adding space; 3.1
+@spaces.route('/spaces/route2', methods=['POST'])
+def add_space():
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    SpaceId = the_data['SpaceId']
+    BuildingId = the_data['BuildingId']
+
+    query = 'insert into Space (SpaceId, BuildingId)'
+    query += str(SpaceId) + '","'
+    query += str(BuildingId) + '","'
+    current_app.logger.info(query)
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return "success!"
+
+
 
 
 #DELETE FROM Space
 #   WHERE SpaceId = 4; -- Removing a space; 3.2
 @spaces.route('/spaces/route3', methods=['DELETE'])
+def delete_space():
+    
+    # collecting data from the request object 
+    the_data = request.json
+    current_app.logger.info(the_data)
+
+    # Constructing the query
+    query = 'DELETE FROM Space WHERE SpaceId = %s'
+    current_app.logger.info(query)
+
+    # executing and committing the insert statement 
+    cursor = db.get_db().cursor()
+    cursor.execute(query)
+    db.get_db().commit()
+
 def remove_space():
     
     # collecting data from the request object 
@@ -38,7 +73,7 @@ def remove_space():
     current_app.logger.info(the_data)
 
     # Constructing the query
-    query = 'DELETE FROM Space WHERE SpacegId = %s'
+    query = 'DELETE FROM Space WHERE SpaceId = %s'
     current_app.logger.info(query)
 
     # executing and committing the insert statement 
@@ -69,7 +104,7 @@ def update_space():
 #FROM Space
 #WHERE isAvailable =True; -- View all available rooms; 3.4
 @spaces.route('/spaces/route5', methods=['GET'])
-def get_avail_spaces():
+def get_avail_spaces_conditions():
     cursor = db.get_db().cursor()
     cursor.execute('select SpaceId, Space.isAvailable as Available \
                     from customers WHERE IsAvailable = TRUE')
