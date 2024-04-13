a8234364653006050007abd185c15ed5bf7b5229
karinamehta2 <karinamehta@yandex.ru>
2024-04-13 17:45:05 -0400

    delete duplicate func delete

--- flask-app/src/spaces/spaces.py
+++ flask-app/src/spaces/spaces.py
@@ -65,21 +65,6 @@ def delete_space():
     cursor = db.get_db().cursor()
     cursor.execute(query)
     db.get_db().commit()
-
-def remove_space():
-    
-    # collecting data from the request object 
-    the_data = request.json
-    current_app.logger.info(the_data)
-
-    # Constructing the query
-    query = 'DELETE FROM Space WHERE SpaceId = %s'
-    current_app.logger.info(query)
-
-    # executing and committing the insert statement 
-    cursor = db.get_db().cursor()
-    cursor.execute(query)
-    db.get_db().commit()
     
     return 'space removed!'
 
